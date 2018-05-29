9f0f213e072e890906a79cb2361057115fe7c564
Alexandr Kostrov <090890kan@gmail.com>
2018-05-29 13:47:33 +0300

    first task done

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,5 +5,19 @@
  * @returns {number}
  */
 function pow (m, n) {
-
+    if (n <= 0 || (n ^ 0) !== n) {
+        
+        return "The wrong input value";
+    } else {
+    var res =m;
+   while (n > 1) {
+       m = m * res;
+       n--;
+   }
+   return m;
+  }
 }
+
+let arg1=+prompt("Enter the first value");
+let arg2=+prompt("Enter the second one");
+alert(pow(arg1,arg2));
