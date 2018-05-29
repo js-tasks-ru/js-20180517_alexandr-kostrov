317f92d2480ce09c2743967c01b76d4f6cc75595
Alexandr Kostrov <090890kan@gmail.com>
2018-05-29 14:42:03 +0300

    second task resolved

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -11,16 +11,20 @@ function print (text) {
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
+function conf (){
+    return true;
+}
 function isValid (name) {
+   return name.length !== 0 && name.indexOf(" ") === -1 && name.length >= 4 && conf();
 }
 
 function sayHello () {
-    let userName = prompt('Введите ваше имя');
+    let userName = prompt('Enter the name');
 
     if (isValid(userName)) {
         print('Welcome back, ' + userName + '!');
     } else {
-        print('Некорректное имя');
+        print('Uncorrect name');
     }
 }
 
