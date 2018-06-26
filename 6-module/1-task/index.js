830d4e1068029b29a8cdbd8aafe68c60e07f8cfd
Alexandr Kostrov <090890kan@gmail.com>
2018-06-26 11:43:32 +0300

    six task resolving final

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,6 +12,13 @@ function getBeforeTime(when) {
         return "The event has already pass away";
     }
     var years = time/1000/60/60/24/365 > 0 ? Math.floor(time/1000/60/60/24/365) : '';
-    var months = (time/1000/60/60/24/365*12 - 12*years) > 0 ? Math.floor(time/1000/60/60/24/365*12 - 12*years) : '';
-    return `${years}years ${months}months`
+    var months = (time/1000/60/60/24/365*12 - 12*years) > 0 ?
+     Math.floor(time/1000/60/60/24/365*12 - 12*years) : '';
+     var days = (time/1000/60/60/24/365*12*30.5 - 12*years*30.5) > 0 ?
+      Math.floor((time/1000/60/60/24/365*12*30.5 - 12*years*30.5)%30.5) : '';
+    var hours =  (time/1000/60/60/24/365*12*30.5*24 - 12*years*30.5*24) > 0 ?
+     Math.floor((time/1000/60/60/24/365*12*30.5*24 - 12*years*30.5*24)%30.5) : '';
+    var minutes = (time/1000/60/60/24/365*12*30.5*24*60 - 12*years*30.5*24*60) > 0 ?
+    Math.floor((time/1000/60/60/24/365*12*30.5*24*60 - 12*years*30.5*24*60)%30.5) : '';
+     return `${years}years ${months}months ${days}days ${hours}hours ${minutes}minutes`
 }
