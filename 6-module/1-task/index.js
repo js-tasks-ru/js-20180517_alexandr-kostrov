1ef373acc5eafb56609d787f35b6e841f62047b4
Alexandr Kostrov <090890kan@gmail.com>
2018-06-26 11:41:29 +0300

    six task resolving

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -6,5 +6,12 @@
  * @return {string} - строка с указанием времени до начала события
  */
 function getBeforeTime(when) {
-    let current = Date.now(); // обязательно получать текущую дату через эту функцию. Иначе тесты работать не будут
+    let current = new Date(Date.now()); // обязательно получать текущую дату через эту функцию. Иначе тесты работать не будут
+   var time = when - current;
+    if((time) < 0){
+        return "The event has already pass away";
+    }
+    var years = time/1000/60/60/24/365 > 0 ? Math.floor(time/1000/60/60/24/365) : '';
+    var months = (time/1000/60/60/24/365*12 - 12*years) > 0 ? Math.floor(time/1000/60/60/24/365*12 - 12*years) : '';
+    return `${years}years ${months}months`
 }
